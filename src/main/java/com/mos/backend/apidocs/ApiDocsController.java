b3140bc0af0f5564a1be8ed0760c6b454a5c58de
sanghoon <tkdgns5817@gmail.com>
2025-03-01 12:15:15 +0900

    Refactor(#5): API Docs 패키지 구조 변경

--- src/main/java/com/mos/backend/apidocs/ApiDocsController.java
+++ src/main/java/com/mos/backend/apidocs/ApiDocsController.java
@@ -1,14 +1,13 @@
-package com.mos.backend.users.presentation.controller.web;
+package com.mos.backend.apidocs;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UserWebController {
+public class ApiDocsController {
 
     @GetMapping("/docs/api")
     public String getApiDocs() {
-        System.out.println("true = " + true);
         return "api.html";
     }
 }
