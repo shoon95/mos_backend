04659bb6a47807a76adf01a87fe4bd225b1c8c09
sanghoon <tkdgns5817@gmail.com>
2025-03-01 21:02:00 +0900

    fix(#4): 엔티티 누락 필드 추가

--- src/main/java/com/mos/backend/studymembers/entity/StudyMember.java
+++ src/main/java/com/mos/backend/studymembers/entity/StudyMember.java
@@ -1,10 +1,14 @@
 package com.mos.backend.studymembers.entity;
 
 import com.mos.backend.common.entity.BaseAuditableEntity;
+import com.mos.backend.studies.entity.Study;
+import com.mos.backend.users.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
@@ -16,4 +20,12 @@ public class StudyMember extends BaseAuditableEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "study_member_id")
     private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(nullable = false, name = "study_id")
+    private Study study;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
 }
