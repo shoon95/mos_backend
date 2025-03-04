eeab8c158943fa7f6494cbee04c6c64012ad2144
LSH <lsh2613@naver.com>
2025-03-05 01:13:16 +0900

    feat: 네이버 oauth2 로그인 구현

---
+++ src/main/java/com/mos/backend/users/application/param/NaverParams.java
@@ -0,0 +1,29 @@
+package com.mos.backend.users.application.param;
+
+import com.mos.backend.users.entity.OauthProvider;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Getter
+@AllArgsConstructor
+public class NaverParams implements OauthParams {
+    private String authorizationCode;
+    @Override
+    public OauthProvider oauthProvider() {
+        return OauthProvider.NAVER;
+    }
+
+    @Override
+    public String getAuthorizationCode() {
+        return authorizationCode;
+    }
+
+    @Override
+    public MultiValueMap<String, String> makeBody() {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", authorizationCode);
+        return body;
+    }
+}
--- src/main/java/com/mos/backend/users/entity/OauthProvider.java
+++ src/main/java/com/mos/backend/users/entity/OauthProvider.java
@@ -2,15 +2,17 @@ package com.mos.backend.users.entity;
 
 import com.mos.backend.users.application.param.GoogleParams;
 import com.mos.backend.users.application.param.KakaoParams;
+import com.mos.backend.users.application.param.NaverParams;
 import com.mos.backend.users.application.param.OauthParams;
 
 public enum OauthProvider {
-    KAKAO, GOOGLE;
+    KAKAO, GOOGLE, NAVER;
 
     public OauthParams getOauthParams(String code) {
         return switch (this) {
             case KAKAO -> new KakaoParams(code);
             case GOOGLE -> new GoogleParams(code);
+            case NAVER -> new NaverParams(code);
         };
     }
 }
