e077ee139f4ace5633cde04dbeef2c9e9fc60661
sanghoon <tkdgns5817@gmail.com>
2025-03-05 20:30:09 +0900

    feat(#15): 검증 추가
    - 시간 검증
    - 스터디 모집 시작 날은 과거일 수 없음
    - 스터디 모집 마감 날이 시작 날보다 먼저일 수 없음

--- src/main/java/com/mos/backend/studies/application/StudyService.java
+++ src/main/java/com/mos/backend/studies/application/StudyService.java
@@ -1,22 +1,21 @@
 package com.mos.backend.studies.application;
 
+import com.mos.backend.common.exception.MosException;
 import com.mos.backend.studies.entity.Category;
 import com.mos.backend.studies.entity.MeetingType;
 import com.mos.backend.studies.entity.Study;
 import com.mos.backend.studies.entity.StudyTag;
+import com.mos.backend.studies.entity.exception.StudyErrorCode;
 import com.mos.backend.studies.infrastructure.StudyRepository;
 import com.mos.backend.studies.presentation.dto.StudyCreateRequestDto;
 import com.mos.backend.studybenefits.application.StudyBenefitService;
 import com.mos.backend.studycurriculum.application.StudyCurriculumService;
-import com.mos.backend.studycurriculum.infrastructure.StudyCurriculumRepository;
 import com.mos.backend.studymembers.application.StudyMemberService;
 import com.mos.backend.studyquestions.application.StudyQuestionService;
 import com.mos.backend.studyrules.application.StudyRuleService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Service
 @RequiredArgsConstructor
@@ -32,6 +31,9 @@ public class StudyService {
     private final StudyMemberService studyMemberService;
 
     public Long create(Long userId, StudyCreateRequestDto requestDto) {
+
+        validateStudyCreateRequest(requestDto);
+
         Study study = Study.builder()
                 .title(requestDto.getTitle())
                 .content(requestDto.getContent())
@@ -57,4 +59,14 @@ public class StudyService {
         return savedStudy.getId();
     }
 
+    private void validateStudyCreateRequest(StudyCreateRequestDto requestDto) {
+        // 모집 날짜 논리적 검증
+        System.out.println("requestDto.getRecruitmentStartDate() = " + requestDto.getRecruitmentStartDate());
+        System.out.println("requestDto.getRecruitmentEndDate() = " + requestDto.getRecruitmentEndDate());
+
+        System.out.println("boolean = " + requestDto.getRecruitmentStartDate().isAfter(requestDto.getRecruitmentEndDate()));
+        if (requestDto.getRecruitmentStartDate().isAfter(requestDto.getRecruitmentEndDate())) {
+            throw new MosException(StudyErrorCode.INVALID_RECRUITMENT_DATES);
+        }
+    }
 }
