d0bec5366eb719b39c7cafac19a5e7a36b407a64
sanghoon <tkdgns5817@gmail.com>
2025-03-08 11:20:51 +0900

    feat(#25): 동시성 문제 테스트 - TestContainer 개발

---
+++ src/test/java/com/mos/backend/testconfig/AbstractTestContainer.java
@@ -0,0 +1,28 @@
+package com.mos.backend.testconfig;
+
+import org.springframework.test.context.DynamicPropertyRegistrar;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class AbstractTestContainer {
+
+    @Container
+    public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username",mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", mySQLContainer::getDriverClassName);
+        registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQL8Dialect");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+    }
+}
