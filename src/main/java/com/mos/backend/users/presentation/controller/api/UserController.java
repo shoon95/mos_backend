6ae07b31cac307f13043b71e66ed3b2778b7f266
LSH <lsh2613@naver.com>
2025-03-08 19:10:15 +0900

    feat: user update api 구현

--- src/main/java/com/mos/backend/users/presentation/controller/api/UserController.java
+++ src/main/java/com/mos/backend/users/presentation/controller/api/UserController.java
@@ -1,8 +1,22 @@
 package com.mos.backend.users.presentation.controller.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.mos.backend.users.application.UserService;
+import com.mos.backend.users.presentation.requestdto.UserUpdateReq;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
+@RequestMapping("/users")
 @RestController
 public class UserController {
+    private final UserService userService;
 
+    @PatchMapping
+    public ResponseEntity<Void> update(@AuthenticationPrincipal Long userId, @Valid @RequestBody UserUpdateReq req) {
+        userService.update(userId, req);
+        return ResponseEntity.ok().build();
+    }
 }
