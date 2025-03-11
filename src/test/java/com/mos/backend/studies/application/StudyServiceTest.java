61d33d8e745be454f8aa897d75299cba7026d526
sanghoon <tkdgns5817@gmail.com>
2025-03-11 22:38:26 +0900

    refactor: 엔티티 변수 명 수정
    - maxParticipants -> maxStudyMemberCount

--- src/test/java/com/mos/backend/studies/application/StudyServiceTest.java
+++ src/test/java/com/mos/backend/studies/application/StudyServiceTest.java
@@ -73,7 +73,7 @@ class StudyServiceTest {
         validRequestDto.setTitle("Test Study");
         validRequestDto.setContent("Study Content");
         validRequestDto.setCategory("프로그래밍");
-        validRequestDto.setMaxParticipants(5);
+        validRequestDto.setMaxStudyMemberCount(5);
         validRequestDto.setRecruitmentStartDate(LocalDate.now());
         validRequestDto.setRecruitmentEndDate(LocalDate.now().plusDays(7));
         validRequestDto.setTags(List.of("Java", "Spring"));
@@ -168,7 +168,7 @@ class StudyServiceTest {
                 .id(1L)
                 .title("testStudy")
                 .content("testContent")
-                .maxParticipantsCount(6)
+                .maxStudyMemberCount(6)
                 .category(Category.PROGRAMMING)
                 .schedule("testSchedule")
                 .recruitmentStartDate(LocalDate.now())
