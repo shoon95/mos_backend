32e4f41a3268c4275115130f7f0f8f8d2847be5f
sanghoon <tkdgns5817@gmail.com>
2025-03-11 22:39:23 +0900

    refactor: @Transactional 수정
    - 기존 jakarta Transactional을 Springframework의 @Transactional로 변경

--- src/main/java/com/mos/backend/studymembers/application/StudyMemberService.java
+++ src/main/java/com/mos/backend/studymembers/application/StudyMemberService.java
@@ -2,25 +2,36 @@ package com.mos.backend.studymembers.application;
 
 import com.mos.backend.common.infrastructure.EntityFacade;
 import com.mos.backend.studies.entity.Study;
+import com.mos.backend.studymembers.entity.ParticipationStatus;
 import com.mos.backend.studymembers.entity.StudyMember;
 import com.mos.backend.studymembers.infrastructure.StudyMemberRepository;
 import com.mos.backend.users.entity.User;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class StudyMemberService {
-
     private final StudyMemberRepository studyMemberRepository;
     private final EntityFacade entityFacade;
 
+    @Transactional
     public void create(Long studyId, Long userId) {
         Study study = entityFacade.getStudy(studyId);
         User user = entityFacade.getUser(userId);
 
         studyMemberRepository.save(StudyMember.create(study, user));
+    }
+
+
+    public int countCurrentStudyMember(Long studyId) {
+        Study study = entityFacade.getStudy(studyId);
+        List<ParticipationStatus> currentParticipationStatusList = Arrays.asList(ParticipationStatus.ACTIVATED, ParticipationStatus.COMPLETED);
+        return studyMemberRepository.countByStudyAndStatusIn(study, currentParticipationStatusList);
     }
 }
--- src/main/java/com/mos/backend/studyrules/application/StudyRuleService.java
+++ src/main/java/com/mos/backend/studyrules/application/StudyRuleService.java
@@ -6,20 +6,21 @@ import com.mos.backend.studies.entity.exception.StudyErrorCode;
 import com.mos.backend.studies.infrastructure.StudyRepository;
 import com.mos.backend.studyrules.entity.StudyRule;
 import com.mos.backend.studyrules.infrastructure.StudyRuleRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class StudyRuleService {
 
     private final StudyRuleRepository studyRuleRepository;
     private final StudyRepository studyRepository;
 
+    @Transactional
     public void create(Long studyId, List<String> contents) {
         if (contents.isEmpty()) {
             return;
