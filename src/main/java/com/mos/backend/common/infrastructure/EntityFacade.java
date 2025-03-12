4736cea43695ea4ce6cad671013a6bd727a01636
LSH <lsh2613@naver.com>
2025-03-12 13:29:34 +0900

    refactor: EntityFacade를 통한 단순 조회 로직 개선

--- src/main/java/com/mos/backend/common/infrastructure/EntityFacade.java
+++ src/main/java/com/mos/backend/common/infrastructure/EntityFacade.java
@@ -1,6 +1,11 @@
 package com.mos.backend.common.infrastructure;
 
 import com.mos.backend.common.exception.MosException;
+import com.mos.backend.studies.entity.Study;
+import com.mos.backend.studies.entity.exception.StudyErrorCode;
+import com.mos.backend.studies.infrastructure.StudyRepository;
+import com.mos.backend.studyparticipations.entity.StudyApplication;
+import com.mos.backend.studyparticipations.infrastructure.StudyApplicationRepository;
 import com.mos.backend.users.entity.User;
 import com.mos.backend.users.entity.exception.UserErrorCode;
 import com.mos.backend.users.infrastructure.respository.UserRepository;
@@ -13,9 +18,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class EntityFacade {
     private final UserRepository userRepository;
+    private final StudyRepository studyRepository;
+    private final StudyApplicationRepository studyApplicationRepository;
 
     public User getUser(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> new MosException(UserErrorCode.USER_NOT_FOUND));
+    }
+
+    public Study getStudy(Long studyId) {
+        return studyRepository.findById(studyId)
+                .orElseThrow(() -> new MosException(StudyErrorCode.STUDY_NOT_FOUND));
+    }
+
+    public StudyApplication getStudyApplication(Long studyApplicationId) {
+        return studyApplicationRepository.findById(studyApplicationId)
+                .orElseThrow(() -> new MosException(StudyErrorCode.STUDY_NOT_FOUND));
     }
 }
--- src/main/java/com/mos/backend/studymembers/application/StudyMemberService.java
+++ src/main/java/com/mos/backend/studymembers/application/StudyMemberService.java
@@ -1,14 +1,10 @@
 package com.mos.backend.studymembers.application;
 
-import com.mos.backend.common.exception.MosException;
+import com.mos.backend.common.infrastructure.EntityFacade;
 import com.mos.backend.studies.entity.Study;
-import com.mos.backend.studies.entity.exception.StudyErrorCode;
-import com.mos.backend.studies.infrastructure.StudyRepository;
 import com.mos.backend.studymembers.entity.StudyMember;
 import com.mos.backend.studymembers.infrastructure.StudyMemberRepository;
 import com.mos.backend.users.entity.User;
-import com.mos.backend.users.entity.exception.UserErrorCode;
-import com.mos.backend.users.infrastructure.respository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,22 +14,13 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class StudyMemberService {
 
-    private final StudyRepository studyRepository;
-    private final UserRepository userRepository;
     private final StudyMemberRepository studyMemberRepository;
+    private final EntityFacade entityFacade;
 
     public void create(Long studyId, Long userId) {
-        Study study = getStudyById(studyId);
-        User user = getUserById(userId);
+        Study study = entityFacade.getStudy(studyId);
+        User user = entityFacade.getUser(userId);
 
         studyMemberRepository.save(StudyMember.create(study, user));
-    }
-
-    private Study getStudyById(Long studyId) {
-        return studyRepository.findById(studyId).orElseThrow(() -> new MosException(StudyErrorCode.STUDY_NOT_FOUND));
-    }
-
-    private User getUserById(Long userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new MosException(UserErrorCode.USER_NOT_FOUND));
     }
 }
