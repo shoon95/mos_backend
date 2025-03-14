5717663bfee6333ef05779a96590dde9cd1e87b9
sanghoon <tkdgns5817@gmail.com>
2025-03-15 04:02:32 +0900

    refactor(#67): 메서드 수정
    - 질문 보기가 없을 때 null이 아닌, 빈 리스트로 반환

--- src/main/java/com/mos/backend/studyquestions/entity/QuestionOption.java
+++ src/main/java/com/mos/backend/studyquestions/entity/QuestionOption.java
@@ -2,13 +2,11 @@ package com.mos.backend.studyquestions.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Embeddable
-@Getter
 public class QuestionOption {
 
     @Column(name = "options", nullable = true)
@@ -23,10 +21,6 @@ public class QuestionOption {
 
     public static QuestionOption fromList(List<String> options) {
         QuestionOption questionOption = new QuestionOption();
-        if (options.isEmpty()) {
-            questionOption.value = null;
-            return questionOption;
-        }
         questionOption.value = String.join(",", options);
         return questionOption;
     }
