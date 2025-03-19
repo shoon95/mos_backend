a0eee639bdf784379e117e8df6eff7611868b7c9
sanghoon <tkdgns5817@gmail.com>
2025-03-19 22:23:23 +0900

    feat(#84): StudyCurriculum 도메인 개발
    - createOrUpdateOrDelete 개발
    - 다 건 조회 개발
    - 단 건 조회 개발
    - tc 작성

--- src/test/java/com/mos/backend/studycurriculum/application/StudyCurriculumServiceTest.java
+++ src/test/java/com/mos/backend/studycurriculum/application/StudyCurriculumServiceTest.java
@@ -1,16 +1,12 @@
 package com.mos.backend.studycurriculum.application;
 
-import com.mos.backend.common.exception.MosException;
 import com.mos.backend.common.infrastructure.EntityFacade;
 import com.mos.backend.studies.entity.Study;
-import com.mos.backend.studies.entity.exception.StudyErrorCode;
-import com.mos.backend.studies.infrastructure.StudyRepository;
-import com.mos.backend.studymembers.application.StudyMemberService;
-import com.mos.backend.studymembers.entity.StudyMember;
-import com.mos.backend.studymembers.infrastructure.StudyMemberRepository;
-import com.mos.backend.users.entity.User;
-import com.mos.backend.users.entity.exception.UserErrorCode;
-import com.mos.backend.users.infrastructure.respository.UserRepository;
+import com.mos.backend.studycurriculum.application.responsedto.StudyCurriculumResponseDto;
+import com.mos.backend.studycurriculum.entity.StudyCurriculum;
+import com.mos.backend.studycurriculum.infrastructure.StudyCurriculumRepository;
+import com.mos.backend.studycurriculum.presentation.requestdto.StudyCurriculumCreateRequestDto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -19,99 +15,131 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@DisplayName("StudyCurriculumService 테스트")
 class StudyCurriculumServiceTest {
-    @Mock
-    private StudyRepository studyRepository;
 
     @Mock
-    private UserRepository userRepository;
-
-    @Mock
-    private StudyMemberRepository studyMemberRepository;
+    private StudyCurriculumRepository studyCurriculumRepository;
 
     @Mock
     private EntityFacade entityFacade;
 
     @InjectMocks
-    private StudyMemberService studyMemberService;
+    private StudyCurriculumService studyCurriculumService;
+
 
     @Nested
-    @DisplayName("스터디 멤버 생성 성공 시나리오")
+    @DisplayName("스터디 커리큘럼 생성, 수정, 삭제 성공 시나리오")
     class SuccessScenarios {
         @Test
-        @DisplayName("정상적인 스터디 멤버 생성")
-        void createStudyMember_Success() {
+        @DisplayName("새로운 스터디 커리큘럼 생성")
+        void createStudyCurriculum_Success() {
             // Given
             Long studyId = 1L;
-            Long userId = 1L;
-            Study mockStudy = mock(Study.class);
-            User mockUser = mock(User.class);
-
+            List<StudyCurriculumCreateRequestDto> contents = List.of(
+                    new StudyCurriculumCreateRequestDto(null, 1L, "커리큘렴 제목 1", "커리큘럼 내용 2"),
+                    new StudyCurriculumCreateRequestDto(null, 2L, "커리큘럼 제목 2", "커리큘럼 내용 2")
+            );
+            Study mockStudy = Study.builder().id(studyId).build();
             when(entityFacade.getStudy(studyId)).thenReturn(mockStudy);
-            when(entityFacade.getUser(userId)).thenReturn(mockUser);
-
+            when(studyCurriculumRepository.findAllByStudy(mockStudy)).thenReturn(new ArrayList<>());
             // When
-            studyMemberService.create(studyId, userId);
+            studyCurriculumService.createOrUpdateOrDelete(studyId, contents);
 
             // Then
-            verify(entityFacade).getStudy(studyId);
-            verify(entityFacade).getUser(userId);
-            verify(studyMemberRepository).save(any(StudyMember.class));
+            verify(studyCurriculumRepository, times(2)).save(any(StudyCurriculum.class));
+        }
+
+        @Test
+        @DisplayName("기존 스터디 커리큘럼 수정")
+        void updateStudyCurriculum_Success() {
+            // Given
+            Long studyId = 1L;
+            Study mockStudy = Study.builder().id(studyId).build();
+
+            StudyCurriculumCreateRequestDto curriculumCreateRequestDto = new StudyCurriculumCreateRequestDto(1L, 1L, "제목", "내용");
+
+            StudyCurriculum mockStudyCurriculum = mock(StudyCurriculum.class);
+            when(entityFacade.getStudy(studyId)).thenReturn(mockStudy);
+            when(studyCurriculumRepository.findByIdAndStudy(studyId, mockStudy)).thenReturn(Optional.of(mockStudyCurriculum));
+
+            // when
+            studyCurriculumService.createOrUpdateOrDelete(studyId, List.of(curriculumCreateRequestDto));
+
+            verify(mockStudyCurriculum).update(curriculumCreateRequestDto.getTitle(), curriculumCreateRequestDto.getSectionId(), curriculumCreateRequestDto.getContent());
+        }
+
+        @Test
+        @DisplayName("기존 스터디 혜택 삭제")
+        void deleteStudyBenefit_Success() {
+            // Given
+            Long studyId = 1L;
+            Study study = Study.builder().id(studyId).build();
+
+            List<StudyCurriculumCreateRequestDto> contents = List.of(
+                    new StudyCurriculumCreateRequestDto(null, 1L, "커리큘렴 제목 1", "커리큘럼 내용 2")
+            );
+
+            StudyCurriculum studyCurriculum1 = spy(StudyCurriculum.create(study, "제목1",1L, "혜택1"));
+            doReturn(1L).when(studyCurriculum1).getId();
+            StudyCurriculum studyCurriculum2 = spy(StudyCurriculum.create(study, "제목2",2L, "혜택2"));
+            doReturn(2L).when(studyCurriculum2).getId();
+
+            when(studyCurriculumRepository.findAllByStudy(study)).thenReturn(new ArrayList<>(List.of(studyCurriculum1, studyCurriculum2)));
+            when(entityFacade.getStudy(studyId)).thenReturn(study);
+
+            // When
+            studyCurriculumService.createOrUpdateOrDelete(studyId, contents);
+
+            // Then
+            verify(studyCurriculumRepository).deleteAll(List.of(studyCurriculum1, studyCurriculum2));
         }
     }
 
-    @Nested
-    @DisplayName("스터디 멤버 생성 실패 시나리오")
-    class FailureScenarios {
-        @Test
-        @DisplayName("스터디가 존재하지 않을 때 MosException 발생")
-        void createStudyMember_StudyNotFound() {
-            // Given
-            Long studyId = 1L;
-            Long userId = 1L;
+    @DisplayName("스터디 단 건 조회 테스트 성공")
+    @Test
+    void getStudyCurriculum_Success() {
 
-            when(entityFacade.getStudy(studyId)).thenThrow(new MosException(StudyErrorCode.STUDY_NOT_FOUND));
+        // given
+        Long studyId = 1L;
+        Long studyCurriculumId = 1L;
 
-            // When & Then
-            MosException exception = assertThrows(MosException.class, () -> {
-                studyMemberService.create(studyId, userId);
-            });
+        Study study = Study.builder().id(studyId).build();
+        StudyCurriculum studyCurriculum = StudyCurriculum.create(study, "제목", 1L, "내용");
+        when(entityFacade.getStudy(studyId)).thenReturn(study);
+        when(studyCurriculumRepository.findByIdAndStudy(studyCurriculumId, study)).thenReturn(Optional.of(studyCurriculum));
 
-            assertEquals(StudyErrorCode.STUDY_NOT_FOUND, exception.getErrorCode());
-            verify(entityFacade, never()).getUser(userId);
-            verify(entityFacade).getStudy(studyId);
-            verify(studyMemberRepository, never()).save(any(StudyMember.class));
-        }
+        // when
+        StudyCurriculumResponseDto responseDto = studyCurriculumService.get(studyId, studyCurriculumId);
 
-        @Test
-        @DisplayName("유저가 존재하지 않을 때 MosException 발생")
-        void createStudyMember_UserNotFound() {
-            // Given
-            Long studyId = 1L;
-            Long userId = 1L;
-            Study mockStudy = mock(Study.class);
+        // then
+        Assertions.assertThat(responseDto.getSectionId()).isEqualTo(1L);
+        Assertions.assertThat(responseDto.getTitle()).isEqualTo("제목");
+        Assertions.assertThat(responseDto.getContent()).isEqualTo("내용");
+    }
 
-            when(entityFacade.getStudy(studyId)).thenReturn(mockStudy);
-            when(entityFacade.getUser(userId)).thenThrow(new MosException(UserErrorCode.USER_NOT_FOUND));
+    @DisplayName("스터디 다 건 조회 테스트 성공")
+    @Test
+    void getAllStudyCurriculums_Success() {
 
-            // When & Then
-            MosException exception = assertThrows(MosException.class, () -> {
-                studyMemberService.create(studyId, userId);
-            });
+        // given
+        Long studyId = 1L;
 
-            assertEquals(UserErrorCode.USER_NOT_FOUND, exception.getErrorCode());
-            verify(entityFacade).getStudy(studyId);
-            verify(entityFacade).getUser(userId);
-            verify(studyMemberRepository, never()).save(any(StudyMember.class));
-        }
+        Study study = Study.builder().id(studyId).build();
+        StudyCurriculum studyCurriculum1 = StudyCurriculum.create(study, "제목1", 1L, "내용1");
+        StudyCurriculum studyCurriculum2 = StudyCurriculum.create(study, "제목2", 2L, "내용2");
+        when(entityFacade.getStudy(studyId)).thenReturn(study);
+        when(studyCurriculumRepository.findAllByStudy(study)).thenReturn(new ArrayList<>(List.of(studyCurriculum1, studyCurriculum2)));
+        // when
+        List<StudyCurriculumResponseDto> responseDto = studyCurriculumService.getAll(studyId);
+
+        // then
+        Assertions.assertThat(responseDto).hasSize(2);
     }
 }
