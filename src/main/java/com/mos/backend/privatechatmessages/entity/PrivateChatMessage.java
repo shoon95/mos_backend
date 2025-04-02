445f5baab8fac3c02eb42990724f227b1756a230
seungheonlee <lsh2613@naver.com>
2025-04-02 21:46:50 +0900

    refactor: 채팅방 엔티티를 1:1과 단톡방 엔티티로 분리

--- src/main/java/com/mos/backend/privatechatmessages/entity/PrivateChatMessage.java
+++ src/main/java/com/mos/backend/privatechatmessages/entity/PrivateChatMessage.java
@@ -1,8 +1,8 @@
-package com.mos.backend.chatmessages.entity;
+package com.mos.backend.privatechatmessages.entity;
 
-import com.mos.backend.chatparticipants.entity.ChatParticipant;
-import com.mos.backend.chatrooms.entity.ChatRoom;
 import com.mos.backend.common.entity.BaseAuditableEntity;
+import com.mos.backend.privatechatrooms.entity.PrivateChatRoom;
+import com.mos.backend.users.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -13,22 +13,22 @@ import static jakarta.persistence.FetchType.LAZY;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "chat_messages")
-public class ChatMessage extends BaseAuditableEntity {
+@Table(name = "private_chat_messages")
+public class PrivateChatMessage extends BaseAuditableEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "chat_message_id")
+    @Column(name = "private_chat_message_id")
     private Long id;
 
     @Column(nullable = false)
     private String message;
 
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(nullable = false, name = "chat_room_id")
-    private ChatRoom chatRoom;
+    @JoinColumn(nullable = false, name = "private_chat_room_id")
+    private PrivateChatRoom privateChatRoom;
 
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(nullable = false, name = "chat_participant_id")
-    private ChatParticipant chatParticipant;
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
 }
