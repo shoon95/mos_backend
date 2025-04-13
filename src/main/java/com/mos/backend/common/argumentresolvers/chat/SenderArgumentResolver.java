98f01e5ce2fdf3f7eb0a5d8b0572a9335f3f4788
LSH <104637774+lsh2613@users.noreply.github.com>
2025-04-13 23:09:32 +0900

    Revert "refactor: stomp userId 추출 방법 수정"

--- src/main/java/com/mos/backend/common/argumentresolvers/chat/SenderArgumentResolver.java
+++ src/main/java/com/mos/backend/common/argumentresolvers/chat/SenderArgumentResolver.java
@@ -1,19 +1,14 @@
 package com.mos.backend.common.argumentresolvers.chat;
 
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.mos.backend.common.annotation.Sender;
-import com.mos.backend.common.jwt.TokenUtil;
-import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
 @Component
 public class SenderArgumentResolver implements HandlerMethodArgumentResolver {
-    private final TokenUtil tokenUtil;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -23,9 +18,7 @@ public class SenderArgumentResolver implements HandlerMethodArgumentResolver {
     @Override
     public Object resolveArgument(MethodParameter parameter, Message<?> message) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-        String accessToken = tokenUtil.extractAccessToken(accessor);
-        DecodedJWT decodedJWT = tokenUtil.decodedJWT(accessToken);
-        Long userId = decodedJWT.getClaim("id").asLong();
+        Object userId = accessor.getSessionAttributes().get("userId");
         return userId;
     }
 }
