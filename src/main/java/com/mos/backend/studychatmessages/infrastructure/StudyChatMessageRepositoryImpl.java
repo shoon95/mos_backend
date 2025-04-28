3323f9b11e233489af4c900f5cc4934960e224e9
seungheonlee <lsh2613@naver.com>
2025-04-29 03:14:03 +0900

    feat: 스터디 메시지 조회 기능 구현

--- src/main/java/com/mos/backend/studychatmessages/infrastructure/StudyChatMessageRepositoryImpl.java
+++ src/main/java/com/mos/backend/studychatmessages/infrastructure/StudyChatMessageRepositoryImpl.java
@@ -4,13 +4,21 @@ import com.mos.backend.studychatmessages.entity.StudyChatMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Repository
 public class StudyChatMessageRepositoryImpl implements StudyChatMessageRepository {
     private final StudyChatMessageJpaRepository studyChatMessageJpaRepository;
+    private final StudyChatMessageQueryDslRepository studyChatMessageQueryDslRepository;
 
     @Override
     public StudyChatMessage save(StudyChatMessage studyChatMessage) {
         return studyChatMessageJpaRepository.save(studyChatMessage);
+    }
+
+    @Override
+    public List<StudyChatMessage> findAllByChatRoomIdForInfiniteScroll(Long studyChatRoomId, Long lastStudyChatMessageId, Integer size) {
+        return studyChatMessageQueryDslRepository.findAllByChatRoomIdForInfiniteScroll(studyChatRoomId, lastStudyChatMessageId, size);
     }
 }
--- src/main/java/com/mos/backend/studychatmessages/presentation/controller/api/StudyChatMessageController.java
+++ src/main/java/com/mos/backend/studychatmessages/presentation/controller/api/StudyChatMessageController.java
@@ -1,10 +1,16 @@
 package com.mos.backend.studychatmessages.presentation.controller.api;
 
 import com.mos.backend.common.annotation.Sender;
+import com.mos.backend.common.dto.InfinityScrollRes;
 import com.mos.backend.studychatmessages.application.StudyChatMessageService;
+import com.mos.backend.studychatmessages.application.res.StudyChatMessageRes;
 import com.mos.backend.studychatmessages.presentation.req.StudyChatMessagePublishReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -15,5 +21,13 @@ public class StudyChatMessageController {
     @MessageMapping("/study-chat-messages")
     public void publishPrivateChatMessage(@Sender Long userId, StudyChatMessagePublishReq studyChatMessagePublishReq) {
         studyChatMessageService.publish(userId, studyChatMessagePublishReq);
+    }
+
+    @GetMapping("/study-chat-rooms/{studyChatRoomId}/messages")
+    public InfinityScrollRes<StudyChatMessageRes> getStudyChatMessages(@AuthenticationPrincipal Long userId,
+                                                                       @PathVariable Long studyChatRoomId,
+                                                                       @RequestParam(required = false) Long lastStudyChatMessageId,
+                                                                       @RequestParam(defaultValue = "10") Integer size) {
+        return studyChatMessageService.getStudyChatMessages(userId, studyChatRoomId,lastStudyChatMessageId, size);
     }
 }
