20f21bed8703428b44b20c09e3bfd69e2248ae59
sanghoon <tkdgns5817@gmail.com>
2025-05-19 16:03:42 +0900

    refactor(#160): 스터디 생성 시 ID 반환 방식 수정
    - 스터디 ID dto로 반환
    - tc 수정

---
+++ src/main/java/com/mos/backend/studies/application/responsedto/StudyCreateResponseDto.java
@@ -0,0 +1,10 @@
+package com.mos.backend.studies.application.responsedto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StudyCreateResponseDto {
+    private long studyId;
+}
