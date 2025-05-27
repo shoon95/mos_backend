2d1693e35eb599ea394883e8f477ba1ad3f9dbd5
seungheonlee <lsh2613@naver.com>
2025-05-28 00:42:09 +0900

    refactor: 인증 예외 처리 메시지 수정

--- src/main/java/com/mos/backend/common/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/mos/backend/common/exception/CustomAuthenticationEntryPoint.java
@@ -16,8 +16,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-
-        String message = "토큰이 유효하지 않습니다.";
+        String message = "토큰이 유효하지 않거나, 예기치 못한 오류가 발생했습니다.\n관리자에게 문의바랍니다.";
         response.getWriter().write(message);
     }
 }
