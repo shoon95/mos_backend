f4ef2fe81a584ec226fb49ac3257c9477f89409c
seungheonlee <lsh2613@naver.com>
2025-06-03 02:20:49 +0900

    refactor: 유저 정보 조회 응답에 image_url 추가

--- src/main/java/com/mos/backend/users/application/responsedto/UserDetailRes.java
+++ src/main/java/com/mos/backend/users/application/responsedto/UserDetailRes.java
@@ -1,17 +1,27 @@
 package com.mos.backend.users.application.responsedto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.mos.backend.users.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserDetailRes {
-    String nickname;
-    String introduction;
-    String categories;
+    private String nickname;
+    private String introduction;
+    private String categories;
+    private String imagePath;
 
     public static UserDetailRes from(User user) {
-        return new UserDetailRes(user.getNickname(), user.getIntroduction(), user.getCategories());
+        UserDetailRes userDetailRes = new UserDetailRes();
+        userDetailRes.nickname = user.getNickname();
+        userDetailRes.introduction = user.getIntroduction();
+        userDetailRes.categories = user.getCategories();
+        userDetailRes.imagePath = user.getImagePath();
+        return userDetailRes;
     }
 }
