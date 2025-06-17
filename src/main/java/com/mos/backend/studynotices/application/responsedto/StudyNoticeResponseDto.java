9f4059b1df129b9b48acf8f5e5eca9069e2dc85b
sanghoon <tkdgns5817@gmail.com>
2025-06-17 16:51:26 +0900

    feat(#196) : 스터디 공지 관련 dto 개발
    - createRequestDto, updateRequestDto
    - responseDto

---
+++ src/main/java/com/mos/backend/studynotices/application/responsedto/StudyNoticeResponseDto.java
@@ -0,0 +1,46 @@
+package com.mos.backend.studynotices.application.responsedto;
+
+import com.mos.backend.studynotices.entity.StudyNotice;
+import com.mos.backend.users.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class StudyNoticeResponseDto {
+    private long studyNoticeId;
+    private String title;
+    private String content;
+    private boolean pinned;
+    private boolean important;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private long creatorId;
+    private String creatorNickname;
+    private Long modifierId;
+    private String modifierNickname;
+    private long studyId;
+
+    public static StudyNoticeResponseDto of (StudyNotice studyNotice, User creator, User modifier, long studyId) {
+        StudyNoticeResponseDto studyNoticeResponseDto = new StudyNoticeResponseDto();
+        studyNoticeResponseDto.studyNoticeId = studyNotice.getId();
+        studyNoticeResponseDto.title = studyNotice.getTitle();
+        studyNoticeResponseDto.content = studyNotice.getContent();
+        studyNoticeResponseDto.pinned = studyNotice.isPinned();
+        studyNoticeResponseDto.important = studyNotice.isImportant();
+        studyNoticeResponseDto.createdAt = studyNotice.getCreatedAt();
+        studyNoticeResponseDto.modifiedAt = studyNotice.getModifiedAt();
+        studyNoticeResponseDto.creatorId = creator.getId();
+        studyNoticeResponseDto.creatorNickname = creator.getNickname();
+        studyNoticeResponseDto.modifierId = studyNotice.getUpdatedBy();
+        if (modifier == null) {
+            studyNoticeResponseDto.modifierNickname = "알 수 없음";
+        } else {
+            studyNoticeResponseDto.modifierNickname = modifier.getNickname();
+        }
+        studyNoticeResponseDto.studyId = studyId;
+        return studyNoticeResponseDto;
+    }
+}
