22d9669204dd4b03d68c6d42576404c8d4f219e1
sanghoon <tkdgns5817@gmail.com>
2025-06-17 16:47:59 +0900

    feat(#196): 스터디 공지 엔티티 생성
    - update 메서드 구현
    - 중요 공지 헤제 메서드 구현

---
+++ src/main/java/com/mos/backend/studynotices/entity/StudyNotice.java
@@ -0,0 +1,66 @@
+package com.mos.backend.studynotices.entity;
+
+import com.mos.backend.common.entity.BaseAuditableEntity;
+import com.mos.backend.studies.entity.Study;
+import com.mos.backend.users.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "study_notices")
+public class StudyNotice extends BaseAuditableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "study_notice_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "study_id", nullable = false)
+    private Study study;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private boolean pinned;
+
+    @Column(nullable = false)
+    private boolean important;
+
+    public static StudyNotice create(Study study, User user, String title, String content, boolean pinned, boolean important) {
+        StudyNotice studyNotice = new StudyNotice();
+        studyNotice.study = study;
+        studyNotice.user = user;
+        studyNotice.title = title;
+        studyNotice.content = content;
+        studyNotice.pinned = pinned;
+        studyNotice.important = important;
+        return studyNotice;
+    }
+
+    public void  update(String title, String content, boolean pinned, boolean important) {
+        this.title = title;
+        this.content = content;
+        this.pinned = pinned;
+        this.important = important;
+    }
+
+    public void unmarkAsImportant() {
+        this.important = false;
+    }
+
+}
