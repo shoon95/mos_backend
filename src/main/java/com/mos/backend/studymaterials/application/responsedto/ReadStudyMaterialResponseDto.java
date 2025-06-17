70bf93066078a6fcef66414f47d4871e284a39a7
sanghoon <tkdgns5817@gmail.com>
2025-06-18 01:12:35 +0900

    refactor(#207): 스터디 자료 조회 시 업로드 날짜 추가

--- src/main/java/com/mos/backend/studymaterials/application/responsedto/ReadStudyMaterialResponseDto.java
+++ src/main/java/com/mos/backend/studymaterials/application/responsedto/ReadStudyMaterialResponseDto.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,6 +19,8 @@ public class ReadStudyMaterialResponseDto {
     private String filePath;
     private String originalName;
     private Long fileSize;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     public static ReadStudyMaterialResponseDto from(StudyMaterial studyMaterial) {
         ReadStudyMaterialResponseDto responseDto = new ReadStudyMaterialResponseDto();
@@ -28,6 +32,8 @@ public class ReadStudyMaterialResponseDto {
         responseDto.filePath = studyMaterial.getFilePath();
         responseDto.originalName = studyMaterial.getOriginalName();
         responseDto.fileSize = studyMaterial.getFileSize();
+        responseDto.createdAt = studyMaterial.getCreatedAt();
+        responseDto.modifiedAt = studyMaterial.getModifiedAt();
         return responseDto;
     }
 }
