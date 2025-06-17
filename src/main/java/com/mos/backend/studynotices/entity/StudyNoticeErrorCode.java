317de3ece7d0ff765656573318e2b259e6b1c27a
sanghoon <tkdgns5817@gmail.com>
2025-06-17 16:46:58 +0900

    feat(#196): 스터디 공지 관련 에러 정의

---
+++ src/main/java/com/mos/backend/studynotices/entity/StudyNoticeErrorCode.java
@@ -0,0 +1,32 @@
+package com.mos.backend.studynotices.entity;
+
+import com.mos.backend.common.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+
+import java.util.Locale;
+
+@RequiredArgsConstructor
+public enum StudyNoticeErrorCode implements ErrorCode {
+    STUDY_NOTICE_NOT_EXISTS(HttpStatus.NOT_FOUND, "study-notice.not-exists");
+
+
+    private final HttpStatus httpStatus;
+    private final String messageKey;
+
+    @Override
+    public HttpStatus getStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getErrorName() {
+        return this.name();
+    }
+
+    @Override
+    public String getMessage(MessageSource messageSource) {
+        return messageSource.getMessage(messageKey, null, Locale.getDefault());
+    }
+}
