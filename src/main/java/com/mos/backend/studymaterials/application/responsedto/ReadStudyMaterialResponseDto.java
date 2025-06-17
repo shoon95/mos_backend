1abaeb1376a98d49eda7325cd2c5eba947a26767
sanghoon <tkdgns5817@gmail.com>
2025-06-18 00:29:43 +0900

    refactor(#207): 스터디 자료 조회 api 수정
    - 다 건 조회, 단 건 조회 dto에 userNickname 추가
    - N + 1 문제 해결 위해  EntityGraph 적용

--- src/main/java/com/mos/backend/studymaterials/application/responsedto/ReadStudyMaterialResponseDto.java
+++ src/main/java/com/mos/backend/studymaterials/application/responsedto/ReadStudyMaterialResponseDto.java
@@ -13,6 +13,7 @@ public class ReadStudyMaterialResponseDto {
     private Long studyId;
     private Long studyMemberId;
     private Long userId;
+    private String userNickname;
     private String filePath;
     private String originalName;
     private Long fileSize;
@@ -23,6 +24,7 @@ public class ReadStudyMaterialResponseDto {
         responseDto.studyId = studyMaterial.getStudy().getId();
         responseDto.studyMemberId = studyMaterial.getStudyMember().getId();
         responseDto.userId = studyMaterial.getStudyMember().getUser().getId();
+        responseDto.userNickname = studyMaterial.getStudyMember().getUser().getNickname();
         responseDto.filePath = studyMaterial.getFilePath();
         responseDto.originalName = studyMaterial.getOriginalName();
         responseDto.fileSize = studyMaterial.getFileSize();
--- src/main/java/com/mos/backend/studymaterials/infrastructure/StudyMaterialJpaRepository.java
+++ src/main/java/com/mos/backend/studymaterials/infrastructure/StudyMaterialJpaRepository.java
@@ -2,8 +2,10 @@ package com.mos.backend.studymaterials.infrastructure;
 
 import com.mos.backend.studies.entity.Study;
 import com.mos.backend.studymaterials.entity.StudyMaterial;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import reactor.util.annotation.NonNull;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,5 +17,10 @@ public interface StudyMaterialJpaRepository extends JpaRepository<StudyMaterial,
     @Query("SELECT SUM(sm.fileSize) FROM StudyMaterial sm WHERE sm.study = :study")
     Optional<Long> sumTotalFileSizeByStudy(Study study);
 
+    @EntityGraph(attributePaths = {"studyMember", "studyMember.user"})
     List<StudyMaterial> findByStudy(Study study);
+
+    @EntityGraph(attributePaths = {"studyMember", "studyMember.user"})
+    @NonNull
+    Optional<StudyMaterial> findById(@NonNull Long studyMaterialId);
 }
