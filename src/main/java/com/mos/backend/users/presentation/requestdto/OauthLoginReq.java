9979fe34b0e6ac7b85f6028da6bab4cb583134b6
sanghoon <tkdgns5817@gmail.com>
2025-08-11 00:30:17 +0900

    fix(#240): oauthLoginReq 기본 생성자 추가

--- src/main/java/com/mos/backend/users/presentation/requestdto/OauthLoginReq.java
+++ src/main/java/com/mos/backend/users/presentation/requestdto/OauthLoginReq.java
@@ -3,9 +3,11 @@ package com.mos.backend.users.presentation.requestdto;
 import com.mos.backend.users.entity.OauthProvider;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class OauthLoginReq {
     private String code;
     private OauthProvider oauthProvider;
