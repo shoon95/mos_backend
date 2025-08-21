9fee34c9a1291e25215753036e6538b9e0df2408
seungheonlee <lsh2613@naver.com>
2025-08-21 17:05:05 +0900

    refactor: 채팅 메시지 조회 조건 추가
    - 삭제된 채팅방의 메시지 조회 시, 삭제 이후의 메시지만 조회되도록 수정

--- src/main/java/com/mos/backend/privatechatmessages/infrastructure/PrivateChatMessageQueryDslRepository.java
+++ src/main/java/com/mos/backend/privatechatmessages/infrastructure/PrivateChatMessageQueryDslRepository.java
@@ -1,13 +1,14 @@
 package com.mos.backend.privatechatmessages.infrastructure;
 
 import com.mos.backend.privatechatmessages.entity.PrivateChatMessage;
-import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 import static com.mos.backend.privatechatmessages.entity.QPrivateChatMessage.privateChatMessage;
 
@@ -16,16 +17,24 @@ import static com.mos.backend.privatechatmessages.entity.QPrivateChatMessage.pri
 public class PrivateChatMessageQueryDslRepository {
     private final JPAQueryFactory queryFactory;
 
-    public List<PrivateChatMessage> findByChatRoomIdForInfiniteScroll(Long privateChatRoomId, Long lastPrivateChatMessageId, int size) {
+    public List<PrivateChatMessage> findByChatRoomIdForInfiniteScroll(Long privateChatRoomId,
+                                                                      Long lastPrivateChatMessageId,
+                                                                      int size,
+                                                                      LocalDateTime deletedAt) {
         return queryFactory
                 .selectFrom(privateChatMessage)
                 .where(
                         eqPrivateChatRoomId(privateChatRoomId),
-                        ltPrivateChatMessageId(lastPrivateChatMessageId)
+                        ltPrivateChatMessageId(lastPrivateChatMessageId),
+                        gtPrivateChatMessageCreatedAt(deletedAt)
                 )
                 .orderBy(privateChatMessage.id.desc())
                 .limit(size + 1)
                 .fetch();
+    }
+
+    private static BooleanExpression gtPrivateChatMessageCreatedAt(LocalDateTime deletedAt) {
+        return Objects.isNull(deletedAt) ? null : privateChatMessage.createdAt.gt(deletedAt);
     }
 
     private static BooleanExpression eqPrivateChatRoomId(Long privateChatRoomId) {
