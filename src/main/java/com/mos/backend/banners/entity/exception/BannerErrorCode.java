6a33d881fc38bc77a797d1a3665f98c6d5df7b2f
sanghoon <tkdgns5817@gmail.com>
2025-08-31 18:52:15 +0900

    feat(#281): 배너 관련 에러 처리 추가

---
+++ src/main/java/com/mos/backend/banners/entity/exception/BannerErrorCode.java
@@ -0,0 +1,34 @@
+package com.mos.backend.banners.entity.exception;
+
+import com.mos.backend.common.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+
+import java.util.Locale;
+
+@RequiredArgsConstructor
+public enum BannerErrorCode implements ErrorCode {
+    NOT_FOUND(HttpStatus.NOT_FOUND, "banner.not-found"),
+    SORT_ORDER_NOT_PROPER(HttpStatus.BAD_REQUEST, "banner.sort-order.not-proper")
+    ;
+
+
+    private final HttpStatus httpStatus;
+    private final String messageKey;
+
+    @Override
+    public HttpStatus getStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getErrorName() {
+        return this.name();
+    }
+
+    @Override
+    public String getMessage(MessageSource messageSource) {
+        return messageSource.getMessage(messageKey, null, Locale.getDefault());
+    }
+}
