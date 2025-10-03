f0bccfa446ade9cea874149288ef8e980423ff12
sanghoon <tkdgns5817@gmail.com>
2025-10-03 17:21:45 +0900

    feat(#286): 알림 저장 쿼리
    - 안읽은 메시지 수 조회
    - 전체 메시지 조회 (페이징, 읽음 상태에 따른 필터링)

--- src/main/java/com/mos/backend/notifications/infrastructure/notificationlog/NotificationLogJpaRepository.java
+++ src/main/java/com/mos/backend/notifications/infrastructure/notificationlog/NotificationLogJpaRepository.java
@@ -4,4 +4,6 @@ import com.mos.backend.notifications.entity.NotificationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationLogJpaRepository extends JpaRepository<NotificationLog, Long>{
+
+    Integer countNotificationLogByRecipientIdAndReadIsFalse(Long userId);
 }
--- src/main/java/com/mos/backend/notifications/infrastructure/notificationlog/NotificationLogRepositoryImpl.java
+++ src/main/java/com/mos/backend/notifications/infrastructure/notificationlog/NotificationLogRepositoryImpl.java
@@ -1,7 +1,11 @@
 package com.mos.backend.notifications.infrastructure.notificationlog;
 
+import com.mos.backend.notifications.application.dto.NotificationResponseDto;
 import com.mos.backend.notifications.entity.NotificationLog;
+import com.mos.backend.notifications.entity.NotificationReadStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +14,7 @@ import java.util.Optional;
 @Repository
 public class NotificationLogRepositoryImpl implements NotificationLogRepository{
     private final NotificationLogJpaRepository notificationLogJpaRepository;
+    private final NotificationLogQueryDslRepository notificationLogQueryDslRepository;
 
     @Override
     public void save(NotificationLog notificationLog) {
@@ -19,5 +24,15 @@ public class NotificationLogRepositoryImpl implements NotificationLogRepository{
     @Override
     public Optional<NotificationLog> findById(Long notificationId) {
         return notificationLogJpaRepository.findById(notificationId);
+    }
+
+    @Override
+    public Integer getUnreadCount(Long userId) {
+        return notificationLogJpaRepository.countNotificationLogByRecipientIdAndReadIsFalse(userId);
+    }
+
+    @Override
+    public Page<NotificationResponseDto> getNotifications(Pageable pageable, Long userId, NotificationReadStatus readStatus) {
+        return notificationLogQueryDslRepository.getNotificationLogs(pageable, userId, readStatus);
     }
 }
